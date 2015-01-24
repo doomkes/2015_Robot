2d765657804289a7116bbc01d80613a2a19a75d3
Matthew1998 <matthew@boersema.org>
2015-01-24 11:26:31 -0500

    Strafe with lift

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -4,13 +4,12 @@ class Robot: public IterativeRobot
 {
 	RobotDrive tank; //normal drive wheels tank drive
 	Joystick lStick, rStick, liftStick;
-	//Talon fMiddle, bMiddle; //strafe motors
-	Talon lift1, lift2;
-	//int frontVal = 0;
-	//int rearVal = 0;
+	Victor lift, fStrafe1, fStrafe2, bStrafe1, bStrafe2;
+	int frontVal = 0;
+	int rearVal = 0;
 	double leftJoy = 0;
 	double rightJoy = 0;
-	//Solenoid front, back;
+	Solenoid front, back;
 
 public:
 	Robot() :
@@ -18,12 +17,13 @@ public:
 			lStick(0),
 			rStick(1),
 			liftStick(2),
-			lift1(6),
-			lift2(7)
-			//fMiddle(6),
-			//bMiddle(7),
-			//front(1),
-			//back(2)
+			lift(6),
+			fStrafe1(2),
+			fStrafe2(3),
+			bStrafe1(6),
+			bStrafe2(5),
+			front(1),
+			back(2)
 	{
 		tank.SetExpiration(0.1);
 	}
@@ -52,11 +52,8 @@ void TeleopPeriodic()
 {
 	leftJoy = lStick.GetY();
 	rightJoy = rStick.GetY();
-	tank.TankDrive(-leftJoy, -rightJoy, true);
-	lift1.Set(liftStick.GetY()*0.5);
-	lift2.Set(liftStick.GetY()*0.5);
+	lift.Set(liftStick.GetY());
 
-/*
 	if(lStick.GetRawButton(3))
 	{
 		frontVal = 1;
@@ -66,24 +63,25 @@ void TeleopPeriodic()
 	}
 	else if (rStick.GetRawButton(3))
 	{
-			frontVal = -1;
-			rearVal = 1;
-			front.Set(true);
-			back.Set(true);
+		frontVal = -1;
+		rearVal = 1;
+		front.Set(true);
+		back.Set(true);
 	}
 	else
-		{
-			frontVal = 0;
-			rearVal = 0;
-			front.Set(false);
-			back.Set(false);
-			tank.TankDrive(-leftJoy, -rightJoy, true);
+	{
+		frontVal = 0;
+		rearVal = 0;
+		front.Set(false);
+		back.Set(false);
+		tank.TankDrive(-leftJoy, -rightJoy, true);
 
-		}
+	}
 
-	fMiddle.Set(frontVal);
-	bMiddle.Set(rearVal);
-*/
+	fStrafe1.Set(frontVal);
+	fStrafe2.Set(frontVal);
+	bStrafe1.Set(rearVal);
+	bStrafe2.Set(frontVal);
 
 }
 
